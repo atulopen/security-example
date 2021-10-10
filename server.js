d6233c421595c8f77975879016ac9727a2c0fddb
Arthur Atul <arthur.atul@ithands.biz>
2021-10-10 17:29:29 +0530

    added helmet for node security

--- server.js
+++ server.js
@@ -2,12 +2,15 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
 const PORT = 3000;
 
-app.use('/secure', (req, res) => {
-    res.send('This is secure page');
+app.use(helmet());
+
+app.use('/secret', (req, res) => {
+    res.send('Your personal secret value is 42');
 });
 
 app.use('/', (req, res) => {
